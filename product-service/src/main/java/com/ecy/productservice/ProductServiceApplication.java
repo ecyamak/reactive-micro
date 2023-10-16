1bd42e1215c88a5b501b27d6d727d0906708b56a
ecyamak <canyamak@ECY-MacBook-Air.local>
2023-10-16 04:58:30 +0300

    docker ci

--- product-service/src/main/java/com/ecy/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecy/productservice/ProductServiceApplication.java
@@ -1,9 +1,12 @@
 package com.ecy.productservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product Service", version = "1.0.0", description = "REST API Documentation"))
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
